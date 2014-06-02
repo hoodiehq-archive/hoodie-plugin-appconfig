627984fe9ac14e590ad6bfd6e74b8696c7e7cb99
Alex Feyerke <kontakt@momoi.de>
2014-06-02 18:08:05 +0200

    Rewrote plugin panel to use the new uikit

--- pocket/assets/js/appconfig.js
+++ pocket/assets/js/appconfig.js
@@ -25,11 +25,56 @@ $(function () {
         $('[name=emailPassword]').val(doc.config.email_pass);
     });
 
+    function setSubmitButtonToSaving(form){
+        $btn = $(form).find('button[type="submit"]');
+        $btn.data('originalButtonText', $btn.text());
+        $btn.data('disabled', 'disabled');
+        $btn.text('Saving');
+    }
+
+    function setSubmitButtonToSuccess(form){
+        $btn.text('Successfully saved!').addClass('success');
+        _.delay(function(){
+            $(form).find('button[type="submit"]').data('disabled', null);
+            $btn.text($btn.data('originalButtonText')).removeClass('success');;
+        }, 2000);
+    }
+
+    function setSubmitButtonToError(form, error){
+        $btn.text('Something went wrong, sorry.').addClass('error');
+        $btn.after('<p class="help-block">'+error+'</p>');
+        _.delay(function(){
+            $(form).find('button[type="submit"]').data('disabled', null);
+            $btn.text($btn.data('originalButtonText')).removeClass('error');;
+        }, 2000);
+    }
+
+    // save app info on submit
+    $('#appInfoForm').submit(function (ev) {
+        var el = this;
+        ev.preventDefault();
+        setSubmitButtonToSaving(this);
+        var cfg = {
+            app_name: $('[name=appName]').val()
+        };
+        updateConfig(cfg, function (err) {
+            if (err) {
+                setSubmitButtonToError(el, err);
+                //return alert(err);
+            }
+            else {
+                setSubmitButtonToSuccess(el);
+            }
+        });
+        return false;
+    });
+
     // save config on submit
     $('#emailForm').submit(function (ev) {
+        var el = this;
         ev.preventDefault();
+        setSubmitButtonToSaving(this);
         var cfg = {
-            app_name: $('[name=appName]').val(),
             email_from: $('[name=fromEmail]').val(),
             email_host: null,
             email_port: null,
@@ -43,7 +88,7 @@ $(function () {
                 return alert(err);
             }
             else {
-                alert('Config saved');
+                setSubmitButtonToSuccess(el);
             }
         });
         return false;
